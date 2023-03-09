151f78c4bd77a30d8ef87cecf9da2bd5acc67eb6
JosephBerman <berman.jos@northeastern.edu>
2023-03-09 04:02:46 -0500

    gets sensor data correctly, now i need to get it cleaned up and with crc checkinga nd etc

--- DHT20.cpp
+++ DHT20.cpp
@@ -10,6 +10,7 @@ int main() {
 
 
     i2c_init(i2c_default, 100000);
+
     printf("i2c init pass\n");
 
 
@@ -18,7 +19,9 @@ int main() {
 
     sensor.intializeRegisters();
     printf("init registers\n");
-    if(!sensor.isReady()){
+    sleep_ms(100);
+    while(!sensor.isReady()){
+
         printf("sensor is not ready waiting 50ms");
         sleep_ms(50);
     }
@@ -27,7 +30,7 @@ int main() {
 
     while(true) {
         sensor.getData(data);
-        printf("tempature: %f , humidity: %f", data.tempature, data.humidity);
+        printf("tempature: %f , humidity: %f\n", data.tempature, data.humidity);
         sleep_ms(1000);
     }
     printf("Done.\n");
@@ -38,59 +41,46 @@ int main() {
 }
 
 void DHT20::getData(DHT_Data &data) {
-    printf("in get data\n");
-    uint8_t outBuf[2];
-    outBuf[0] = 0x33;
-    outBuf[1] = 0x00;
+    uint8_t outBuf[3]=  {0xAC, 0x33, 0x00};
 
-    printf("requesting data\n");
-    i2c_write_blocking(i2c_default,0xAC ,outBuf, 2, false);
+    i2c_write_blocking(i2c_default,_I2CAddress ,outBuf, 3, false);
+
     sleep_ms(50);
 
     uint8_t inBuf[7];
-    printf("sent message, reading data\n");
-    i2c_read_raw_blocking(i2c_default,inBuf, 7);
-    printf("received data\n");
+    i2c_read_blocking(i2c_default,_I2CAddress, inBuf, 7, false);
 
 
-    float humidity = inBuf[1] << 12 | inBuf[2] << 4 | inBuf[3] >> 4;
-    float tempature = (inBuf[3] << 16 | inBuf[4] << 8 | inBuf[5]) &  0xffff;
-    humidity = (humidity / pow(2, 20)) * 100;
-    tempature = ((tempature / pow(2,20)) * 200) - 50;
+    uint32_t humidity = inBuf[1] << 12 | inBuf[2] << 4 | (inBuf[3]&0xf0) >> 4;
+    uint32_t tempature = (inBuf[3]&0xf) << 16 | inBuf[4] << 8 | inBuf[5];
 
-    data.humidity = humidity;
-    data.tempature = tempature;
+
+    data.humidity = humidity * 9.5367431640625e-5;;
+    data.tempature = tempature * 1.9073486328125e-4 - 50;
 
 
 }
 
 bool DHT20::isReady() {
     uint8_t buf[1];
-    buf[0] = 0x71;
 
-    i2c_write_raw_blocking(i2c_default, buf, 1);
-    i2c_read_raw_blocking(i2c_default, buf, 1);
+    i2c_read_blocking(i2c_default, _I2CAddress, buf, 1, false);
 
     if(buf[0] == 0x18){
+
+
         return true;
     }
-    else{
-        printf("DHT20 is NOT ready\n");
-        return false;
-    }
+    return false;
 }
 
 void DHT20::intializeRegisters() {
-    printf("resetting registers\n");
-    uint8_t buf[1];
-    buf[0] = 0;
-    printf("sending reset to 0x1b\n");
+    uint8_t reset1[3] = {0x1B,0x00,0x00};
+    uint8_t reset2[3] = {0x1C,0x00,0x00};
+    uint8_t reset3[3] = {0x1E,0x00,0x00};
 
-    i2c_write_blocking(i2c_default, 0x1B, buf,1, false);
-    printf("sending reset to 0x1c\n");
-    i2c_write_blocking(i2c_default, 0x1c, buf,1, false);
-    printf("sending reset to 0x1e\n");
-    i2c_write_blocking(i2c_default, 0x1e, buf,1, false);
-    printf("finshed resetting");
+    i2c_write_blocking(i2c_default, _I2CAddress, reset1,1, false);
+    i2c_write_blocking(i2c_default, _I2CAddress, reset2,1, false);
+    i2c_write_blocking(i2c_default, _I2CAddress, reset3,1, false);
 }
 
